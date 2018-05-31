8d51e8c4af0991a9e67fd6e530fe78b4b441b055
Michael Liendo <model3volution@gmail.com>
2018-05-31 18:02:43 -0500

    push final

--- src/index.js
+++ src/index.js
@@ -1,19 +1,71 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import './style.css'
+import {fetchRepos} from './github'
 
 class App extends Component {
 
   state = {
-    name:'Michael'
+    name:'Michael',
+    repos: []
   }
+
+  handleSubmit = (username) => {
+    fetchRepos(username)
+    .then(repos => {
+      const repoNames = repos.map(repo => repo.name)
+      this.setState({repos: repoNames})
+    })
+  }
+
+  renderList = () => {
+    return this.state.repos.map(repo => {
+      return <li>{repo}</li>
+    })
+  }
+
   render() {
     return (
       <div className='app'>
-      {this.state.name}
+      <h1>Repo Lister</h1>
+      <SearchForm onSubmit={this.handleSubmit}/>
+      <ul>{this.renderList()}</ul>
       </div>
     )
   }
 }
+
+class SearchForm extends Component {
+  
+  state = {
+    searchText: ''
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.onSubmit(this.state.searchText)
+  }
+
+  handleChange = (e) => {
+    const value = e.target.value
+
+    this.setState({
+      searchText: value
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          value={this.state.searchText} 
+          placeholder={'enter a username'}
+          onChange= {this.handleChange}
+        />
+        <button>Click me!</button>
+      </form>
+    )
+  }
+}
+
 
 render(<App/>, document.querySelector('#root'))
